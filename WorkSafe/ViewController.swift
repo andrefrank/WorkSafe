ce8013d5b10afdc7ade4d8d2656c74118ff4fadc
Andre Frank <andrefrank.mail@t-online.de>
2019-04-22 14:46:47 +0200

    Initial Commit

---
+++ WorkSafe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkSafe
+//
+//  Created by Andre Frank on 22.04.19.
+//  Copyright © 2019 Afapps+. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
